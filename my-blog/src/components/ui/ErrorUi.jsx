b3ff6a7fa594c54ce66cb8d1fc90e689fc3e8f41
Benjamin benjamin chidera <136095035+Benjamin-chidera@users.noreply.github.com>
2023-10-23 07:44:36 +0100

    Delete my-blog directory

--- my-blog/src/components/ui/ErrorUi.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const ErrorUi = () => {
-  return (
-    <div>ErrorUi</div>
-  )
-}
--- my-blog/src/components/ui/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Footer = () => {
-  return (
-    <div className=' text-gray-700 text-center text-xl bg-gray-800 p-5'>
-        <h1>&copy; Blog 2023. Published by Benjamin</h1>
-    </div>
-  )
-}
